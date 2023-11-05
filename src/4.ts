7e002599f0103952a501d6f13fa1286a6c1be123
DarynaSD <dasha.dermanchuk@gmail.com>
2023-11-05 13:58:11 +0200

    4

--- src/4.ts
+++ src/4.ts
@@ -1,7 +1,47 @@
+class Key {
+    private signature = Math.random();
+    public getSignature(): number {
+        // console.log(this.signature);
+        return this.signature;
+  }
+}
+
+class Person {
+    constructor(private key: Key, public name: string) { }
+    public getKey(): Key {
+        // console.log(this.key);
+        return this.key;
+    }
+}
+
+abstract class House {
+    constructor(public key: Key) { }
+    public door: boolean;
+    public tenants: Person[];
+
+    public comeIn(man: Person): void | never {
+        this.door ? this.tenants.push(man) : alert(new Error('Door is closed'));
+    };
+
+    public abstract openDoor(personKey: Key): void;
+
+}
+
+class MyHouse extends House {
+    public  openDoor(personKey: Key): void{
+    personKey === this.key ? this.door = true : this.door = false
+    }
+}
+
+
+
+
+
+//
 const key = new Key();
 
 const house = new MyHouse(key);
-const person = new Person(key);
+const person = new Person(key, "Bob");
 
 house.openDoor(person.getKey());
 
